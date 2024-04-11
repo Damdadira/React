fa7366ec83ec22815e1751187971e221cab3eb76
Damdadira <suk971029@gmail.com>
2024-04-11 20:23:00 +0900

    make Clock

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,16 +4,26 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import Library from "./Chapter_03/Library";
+/**Chapter_03 Code */
+// import Library from "./Chapter_03/Library";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <Library />
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <Library />
+//     </React.StrictMode>,
+//     document.getElementById("root")
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+/**Chapter_04 Code */
+import Clock from "./Chapter_04/Clock";
+
+setInterval(() => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <Clock />
+        </React.StrictMode>,
+        document.getElementById("root")
+    );
+}, 1000);
+
 reportWebVitals();
