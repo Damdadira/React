3a002eb9b1a5dbd437c61c797d0a731e165ef611
Damdadira <suk971029@gmail.com>
2024-04-13 17:21:55 +0900

    Components and Props

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -15,15 +15,25 @@ import reportWebVitals from "./reportWebVitals";
 // );
 
 /**Chapter_04 Code */
-import Clock from "./Chapter_04/Clock";
+// import Clock from "./Chapter_04/Clock";
 
-setInterval(() => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <Clock />
-        </React.StrictMode>,
-        document.getElementById("root")
-    );
-}, 1000);
+// setInterval(() => {
+//     ReactDOM.render(
+//         <React.StrictMode>
+//             <Clock />
+//         </React.StrictMode>,
+//         document.getElementById("root")
+//     );
+// }, 1000);
+
+/**Chapter_05 Code */
+import CommentList from "./Chapter_05/CommentList";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <CommentList></CommentList>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
 
 reportWebVitals();
