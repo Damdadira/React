02a00d6eb2188515a9b1c4ead16a41cd3703ca8a
Damdadira <suk971029@gmail.com>
2024-04-14 19:23:09 +0900

    Handling Events(Class)

---
+++ my-app/src/Chapter_08/ConfirmButton.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+class ConfirmButton extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isConfirmed: false,
+        };
+
+        // this.handleConfirm = this.handleConfirm.bind(this);
+    }
+
+    //bind 사용할때
+    // handleConfirm() {
+    //     this.setState((prevState) => ({
+    //         isConfirmed: !prevState.isConfirmed,
+    //     }));
+    // }
+
+    //class fields syntax 사용할때
+    handleConfirm = () => {
+        this.setState((prevState) => ({
+            isConfirmed: !prevState.isConfirmed,
+        }));
+    };
+
+    render() {
+        return (
+            <button
+                onClick={this.handleConfirm}
+                disabled={this.state.isConfirmed}
+            >
+                {this.state.isConfirmed ? "확인됨" : "확인하기"}
+            </button>
+        );
+    }
+}
+
+export default ConfirmButton;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,54 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-/**Chapter_03 Code */
 // import Library from "./Chapter_03/Library";
-
-// ReactDOM.render(
-//     <React.StrictMode>
-//         <Library />
-//     </React.StrictMode>,
-//     document.getElementById("root")
-// );
-
-/**Chapter_04 Code */
 // import Clock from "./Chapter_04/Clock";
-
-// setInterval(() => {
-//     ReactDOM.render(
-//         <React.StrictMode>
-//             <Clock />
-//         </React.StrictMode>,
-//         document.getElementById("root")
-//     );
-// }, 1000);
-
-/**Chapter_05 Code */
 // import CommentList from "./Chapter_05/CommentList";
-
-// ReactDOM.render(
-//     <React.StrictMode>
-//         <CommentList></CommentList>
-//     </React.StrictMode>,
-//     document.getElementById("root")
-// );
-
-/**Chapter_06 Code */
 // import NotificationsList from "./Chapter_06/NotificationList";
+// import Accommodate from "./Chapter_07/Accommondata";
 
-// ReactDOM.render(
-//     <React.StrictMode>
-//         <NotificationsList></NotificationsList>
-//     </React.StrictMode>,
-//     document.getElementById("root")
-// );
-
-/**Chapter_07 Code */
-import Accommodate from "./Chapter_07/Accommondata";
+/**Chapter_08 Code */
+import ConfirmButton from "./Chapter_08/ConfirmButton";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Accommodate></Accommodate>
+        <ConfirmButton></ConfirmButton>
     </React.StrictMode>,
     document.getElementById("root")
 );
