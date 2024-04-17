cdf039f314745e2a14b503679874babc77f29a44
Damdadira <suk971029@gmail.com>
2024-04-17 20:26:23 +0900

    List and Keys

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -10,13 +10,14 @@ import reportWebVitals from "./reportWebVitals";
 // import NotificationsList from "./Chapter_06/NotificationList";
 // import Accommodate from "./Chapter_07/Accommondata";
 // import ConfirmButton from "./Chapter_08/ConfirmButton";
+// import LandingPage from "./Chapter_09/LandingPage";
 
-/**Chapter_09 Code */
-import LandingPage from "./Chapter_09/LandingPage";
+/**Chapter_10 Code */
+import AttendanceBook from "./Chapter_10/AttendanceBook";
 
 ReactDOM.render(
     <React.StrictMode>
-        <LandingPage></LandingPage>
+        <AttendanceBook></AttendanceBook>
     </React.StrictMode>,
     document.getElementById("root")
 );
